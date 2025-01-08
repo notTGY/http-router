6073833db47e8eec1fe9e5f6091c48402874c620
notTGY <microcraft47@gmail.com>
2025-01-08 04:11:18 +0300

    everything except tarm/serial cross compilation

--- pico/main.go
+++ pico/main.go
@@ -2,53 +2,78 @@ package main
 
 import (
 	"machine"
-  "time"
+	"strings"
+	"time"
 )
 
+const MTU = 128
+const MAX_CHUNKS = 32
+
 func main() {
-  time.Sleep(5*time.Second)
+	serial := machine.Serial
 
-  serial := machine.Serial
+	led := machine.LED
+	led.Configure(machine.PinConfig{
+		Mode: machine.PinOutput,
+	})
 
-  led := machine.LED
-  led.Configure(machine.PinConfig{
-    Mode: machine.PinOutput,
-  })
+	led.High()
+	time.Sleep(5 * time.Second)
+	led.Low()
 
-  for {
-    data := make([]byte, 1)
-    _, err := serial.Write([]byte("the name: "))
-    if err != nil {
-      println("Write err: ", err)
-    }
-    blink(led)
+	for {
+		/*
+			_, err := serial.Write([]byte("the name: "))
+			if err != nil {
+				println("Write err: ", err)
+			}
+			blink(led)
+		*/
 
-    for {
-      if serial.Buffered() > 0 {
+    request := ""
+    for i := 0; i < MAX_CHUNKS; i++ {
+      for serial.Buffered() == 0 { }
+      buf := make([]byte, MTU)
+      j := 0
+      for ; serial.Buffered() > 0 && j < MTU; j++ {
         inByte, _ := serial.ReadByte()
-        err = serial.WriteByte(inByte)
+
+        /*
+        err := serial.WriteByte(inByte)
         if err != nil {
           println("WriteByte err: ", err)
         }
-        if inByte == byte(13) {
-          break
-        }
-        data = append(data, inByte)
+        */
+
+        buf[j] = inByte
+      }
+      request = request + string(buf[:j])
+
+      parts := strings.Count(
+        strings.ReplaceAll(request, "\r", ""),
+        "\n\n",
+      )
+      if parts >= 1 {
+        break
       }
     }
-    blink(led)
+		blink(led)
 
-    output := "\r\nThe name is the " + string(data) + "\r\n"
-    _, err = serial.Write([]byte(output))
-    if err != nil {
-      println("Write err: ", err)
-    }
-  }
+		response := `HTTP/1.1 200 OK
+
+Hello, World!
+
+`
+		_, err := serial.Write([]byte(response))
+		if err != nil {
+			println("Write err: ", err)
+		}
+	}
 }
 
 func blink(p machine.Pin) {
-  p.High()
-  time.Sleep(500*time.Millisecond)
-  p.Low()
-  time.Sleep(500*time.Millisecond)
+	p.High()
+	time.Sleep(500 * time.Millisecond)
+	p.Low()
+	time.Sleep(500 * time.Millisecond)
 }
--- router/main.go
+++ router/main.go
@@ -2,81 +2,146 @@ package main
 
 import (
 	"fmt"
-  "strings"
-  "time"
+	"strings"
+	"time"
+  "net"
+  "io"
 
 	"github.com/tarm/serial"
 )
 
+//const DEVICE = "COM4" // Windows
+const DEVICE = "/dev/ttyACM0" // Linux
+const MTU = 246
+const MAX_CHUNKS = 32
+
+func onConnection(
+  pico *serial.Port,
+  //pico net.Conn,
+  conn net.Conn,
+) error {
+  //fmt.Printf("Request:\n")
+	for i := 0; i < MAX_CHUNKS; i++ {
+    buf := make([]byte, MTU)
+    nread, err := conn.Read(buf)
+    if err != nil {
+      if err == io.EOF {
+        break
+      }
+      return err
+    }
+    if nread == 0 {
+      break
+    }
+    //fmt.Printf("[%d]%s", nread, string(buf[:nread]))
+
+    to_write := make([]byte, nread)
+    for idx := 0; idx < nread; idx++ {
+      to_write[idx] = buf[idx]
+    }
+
+    // this breaks if you pass to_write
+    nwrote, err := pico.Write(buf)
+    if err != nil {
+      fmt.Printf("pico write err\n")
+      return err
+    }
+    if nwrote != nread {
+      break
+    }
+    time.Sleep(100*time.Millisecond)
+  }
+  //fmt.Printf("Response:\n")
+
+  response := ""
+	//for i := 0; i < MAX_CHUNKS; i++ {
+  for len(response) < MAX_CHUNKS * MTU {
+    buf := make([]byte, MTU)
+		nread, err := pico.Read(buf)
+    //fmt.Printf("[%d]", nread)
+		if err != nil {
+			fmt.Println("Error reading from pico:", err)
+			return err
+		}
+		if nread > 0 {
+      to_write := make([]byte, nread)
+      for idx := 0; idx < nread; idx++ {
+        to_write[idx] = buf[idx]
+      }
+      nwrote, err := conn.Write(to_write)
+      if err != nil {
+        return err
+      }
+      if nwrote != nread {
+        /*
+        fmt.Printf(
+          "Mismatched write-read: %d-%d\n",
+          nwrote,
+          nread,
+        )
+        */
+      }
+
+			str := strings.ReplaceAll(string(buf[:nread]), "\r", "")
+      //fmt.Printf("%s", str)
+      response = response + str
+      http_part_index := strings.Count(response, "\n\n")
+      if strings.Index(response, "\n") == strings.Index(response, "\n\n") {
+        http_part_index++
+      }
+      N_HTTP_PARTS := 2
+			if http_part_index >= N_HTTP_PARTS {
+        //pico.Flush()
+        //fmt.Printf("Finished\n")
+				return nil
+			}
+		} else {
+      //fmt.Printf("Finished\n")
+      return nil
+    }
+	}
+  //fmt.Printf("Too large\n")
+  return nil
+}
+
 func main() {
 	config := &serial.Config{
-		Name: "COM4", // Change this to your USB port, e.g., "/dev/ttyACM0" on Linux
+		Name: DEVICE,
 		Baud: 115200,
 	}
-  var pico *serial.Port
-  for {
-    var err error
+	var pico *serial.Port
+	//var pico net.Conn
+
+	for {
+		var err error
     pico, err = serial.OpenPort(config)
-    if err != nil {
-      fmt.Println("Error opening serial port:", err)
-      time.Sleep(time.Second)
-      continue
-    }
-    break
-  }
+		//pico, err = net.Dial("unix", DEVICE)
+		if err != nil {
+			fmt.Println("Error opening serial port:", err)
+			time.Sleep(time.Second)
+			continue
+		}
+		break
+	}
 	defer pico.Close()
 
-
-  buf := make([]byte, 32)
-  res := ""
-  count := 0
-  fmt.Printf("Ready\n")
-  for i := 0; i < 10; i++ {
-    n, err := pico.Read(buf)
-    if err != nil {
-      fmt.Println("Error reading from pico:", err)
-      return
-    }
-    if n > 0 {
-      str := string(buf[:n])
-      res = res + str
-      count += n
-      if strings.HasSuffix(res, ": ") {
-        break
-      }
-    }
-  }
-  fmt.Printf("Received %d: \"%s\"\n", count, res)
-
-  if count != 10 {
-    fmt.Printf("Early exit\n")
-    return
-  }
-
-  body := []byte("Primeagen\r\n")
-  n, err := pico.Write(body)
+  ln, err := net.Listen("tcp", ":8000")
   if err != nil {
-    fmt.Println("Error writing to pico:", err)
+    fmt.Println("Error listening:", err)
     return
   }
-  fmt.Printf("Written %d bytes; %v\n", n, body)
-
-  res = ""
-  count = 0
-  for i := 0; i < 10; i++ {
-    n, err := pico.Read(buf)
+	for {
+    conn, err := ln.Accept()
     if err != nil {
-      fmt.Println("Error reading from pico:", err)
+      fmt.Println("Error accepting:", err)
       return
     }
-    if n > 0 {
-      str := string(buf[:n])
-      res = res + str
-      count += n
-      if strings.HasSuffix(res, ": ") {
-        break
-      }
+    // no `go` here, because usb can't multiplex
+		err = onConnection(pico, conn)
+    if err != nil {
+      fmt.Printf("Following error occured:\n")
+      fmt.Println(err)
     }
-  }
-  fmt.Printf("Received %d: \"%s\"", count, res)
+    conn.Close()
+	}
 }
